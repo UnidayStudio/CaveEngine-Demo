10ca8475985777d4e44e5c5698718d0d10abee1d
Uniday Studio <guilhermegt13@gmail.com>
2024-09-12 17:50:36 -0300

    Improved the Vehicle Demo + New Docs
    Added:
    - Gradual Acceleration
    - Engine Sound
    - Particles and Collision Damage Sound
    - I also improved the visuals!

--- Data/Content/Demos/Vehicle/Vehicle (1712739899).py
+++ Data/Content/Demos/Vehicle/Vehicle (1712739899).py
@@ -1,19 +1,39 @@
 import cave
+import cave.math
+import cave.random
 
 class VehicleController(cave.Component):
 	def start(self, scene):
-		self.vehicle = self.entity.get("Vehicle")
+		self.vehicle : cave.VehicleComponent = self.entity.get("Vehicle")
+		self.rigidBody : cave.RigidBodyComponent = self.entity.get("Rigid Body")
 
-	def update(self):
+		# Playing the Engine sound...
+		self.sd = cave.playSound("car-engine-02.ogg", 2, -1)
+
+		# This force variable is a way to don't instantly apply full throttle
+		# to the vehicle. Instead, we slowly increate it as you hold the acc.
+		self.force = 0.0
+
+		self.lastSparkPos = cave.Vector3(0)
+		self.lastSparkTimer = cave.SceneTimer()
+
+	def addForce(self, value):
+		self.force = cave.math.clamp(self.force + value, 0.0, 1.0)
+
+	def movement(self):
+		dt = cave.getDeltaTime()
 		events = cave.getEvents()
 
 		if events.active(cave.event.KEY_W):
-			self.vehicle.accelerate()
+			self.addForce(dt)
+			self.vehicle.accelerate(self.force)
 		elif events.active(cave.event.KEY_S):
-			self.vehicle.reverse()
+			self.addForce(dt * 0.5)
+			self.vehicle.reverse(self.force)
 		else:
+			self.addForce(-dt * 0.5)
 			self.vehicle.idle()
-
+		
 		if events.pressed(cave.event.KEY_LCTRL):
 			self.vehicle.brake()
 		elif events.released(cave.event.KEY_LCTRL):
@@ -26,6 +46,45 @@ class VehicleController(cave.Component):
 		else:
 			self.vehicle.turnStraight()
 		
+	def soundControl(self):		
+		if self.sd.isPaused():
+			self.sd.resume()
+
+		self.sd.pitch  = cave.math.mapRange(self.force, 0, 1, 0.5, 1.0)
+		self.sd.volume = cave.math.mapRange(self.force, 0, 1, 0.7, 2.0)
+
+	def damageControl(self):
+		scene = cave.getScene()
+
+		for col in self.rigidBody.getCollisions():
+			if col.position.length() < 0.5:
+				continue
+			if (self.lastSparkPos - col.position).length() < 2.0 and self.lastSparkTimer.get() < 1.0:
+				continue
+			
+			# Collision Effect:
+			spark = scene.addFromTemplate("Spark", col.position)
+			spark.scheduleKill(0.5)
+
+			# Collision Sound:
+			sd = cave.playSound("crash-01.ogg", 0.5)
+			sd.setSource3D(spark)
+			sd.pitch = cave.random.uniform(0.5, 2.0)
+
+			self.lastSparkPos = col.position.copy()
+			self.lastSparkTimer.reset()
+			break
+
+	def update(self):
+		self.movement()
+		self.soundControl()
+
+		self.damageControl()
+		
+	def pausedUpdate(self):
+		if self.sd.isPlaying():
+			self.sd.pause()
+
 	def end(self, scene):
 		pass
 	
